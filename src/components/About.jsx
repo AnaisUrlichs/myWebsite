29a1bd8f32e4a25706781c764352d8885b6a4f70
unknown <urlichsanais@gmail.com>
2019-06-08 20:27:45 +0200

    Changes to the About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
+import "./About.css";
 
 export default class About extends Component {
   render() {
     return (
-      <div class="hobbies">
-        <h5>Hobbies</h5>
-        <h3>All about a positive work-life balance</h3>
+      <div>
+        <div className="navSpace" />
+        <div className="hobbies">
+          <h5>Hobbies</h5>
+          <h3>All about a positive work-life balance</h3>
 
-        <div className="flex-container" id="hobbies">
-          <div id="cooking">
-            <h5>Cooking</h5>
-          </div>
-          <div id="reading">
-            <h5>A good book and some tea</h5>
-          </div>
-          <div id="travelling">
-            <h5>Travelling</h5>
-          </div>
-          <div id="sports">
-            <h5>Sports</h5>
+          <div className="flex-container" id="hobbies">
+            <div id="cooking">
+              <h5>Cooking</h5>
+            </div>
+            <div id="reading">
+              <h5>A good book and some tea</h5>
+            </div>
+            <div id="travelling">
+              <h5>Travelling</h5>
+            </div>
+            <div id="sports">
+              <h5>Sports</h5>
+            </div>
           </div>
         </div>
       </div>
