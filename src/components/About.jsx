5fd3619206a9a9e16ac8d7eb68ed384e6d3452e8
unknown <urlichsanais@gmail.com>
2019-06-09 17:44:47 +0200

    updates about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,16 @@ export default class About extends Component {
     return (
       <div>
         <div className="navSpace" />
+        <img className="snowImage" src={require("./image/snowImage.png")} />
+
+        <div className="statistics">
+          <h3>Random figures</h3>
+          <h5>countries visited</h5>
+          <h5>pets I have had throughout my life</h5>
+          <h5>Languages I speak</h5>
+        </div>
+
+        <div className="personalGallery" />
         <div className="hobbies">
           <h5>Hobbies</h5>
           <h3>All about a positive work-life balance</h3>
