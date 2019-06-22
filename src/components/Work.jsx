e0f2392ff9fd479548de5b223bea831eabbed7f1
unknown <urlichsanais@gmail.com>
2019-06-22 22:58:07 +0200

    route rendering fixes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "./Work.css";
 import SpeakingVideos from "./extras/SpeakingVideos.jsx";
 import Companies from "./extras/companies.jsx";
